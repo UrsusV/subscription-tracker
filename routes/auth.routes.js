6027d1dac0a83ec0a0f073b1da4c26cd97332b11
UrsusV <mohaimenahmed8@gmail.com>
2025-09-18 18:02:15 +0600

    "defined some basic auth routes"

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,13 +3,15 @@ import { Router } from "express";
 const authRouter = Router();
 
 authRouter.post('/sign-up', (req, res) => {
-  res.send('Sign up');
+  res.send({"title": "Sign up"});
 });
 
 authRouter.post('/sign-in', (req, res) => {
-  res.send('Sign in');
+  res.send({"title": "Sign up"});
 });
 
-
+authRouter.post('/sign-out', (req, res) => {
+  res.send({"title": "Sign out"});
+});
 
 export default authRouter;
